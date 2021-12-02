7be5286f5f0fb67ea1603c0bd7b453e2a3a42c05
kevinagnew <kevin_2@live.co.uk>
2021-12-02 14:41:45 +0000

    Updated task tracker

--- AgrimetricsAssignment/urls.py
+++ AgrimetricsAssignment/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 
 urlpatterns = [
-    path('home', views.home_view, name='homepage')
+    path('home', views.home_view, name='homepage'),
+    path('order_sandwich', views.sandwich_order_view, name='order_sandwich'),
+    path('create_sandwich', views.create_sandwich_order_view, name='create_sandwich'),
+    path('order_delete', views.sandwich_order_delete_view, name='delete_sandwich'),
+    path('task_to_complete', views.task_to_complete_view, name='task_to_complete')
 ]
--- AgrimetricsAssignment/views.py
+++ AgrimetricsAssignment/views.py
@@ -1,6 +1,48 @@
 from django.shortcuts import render
+from AgrimetricsAssignment.create_sandwich_order import CreateSandwich
+
+# Accept sandwich orders
+# List of tasks for giovanni
 
 
 # Request for home_view
 def home_view(request):
     return render(request, 'homepage.html')
+
+
+# Navigate to sandwich order
+def sandwich_order_view(request):
+    return render(request, 'create_order.html')
+
+
+# Request for create_sandwich_order_view
+def create_sandwich_order_view(request):
+    if (request.method == 'POST'):
+        sandwich = {
+            'name': request.POST.get('name'),
+            'base': request.POST.get('base'),
+            'meat_one': request.POST.get('meat_one'),
+            'meat_two': request.POST.get('meat_two'),
+            'cheese': request.POST.get('cheese'),
+            'sauce': request.POST.get('sauce'),
+            'salad': request.POST.get('salad'),
+            'extra': request.POST.get('extra')
+        }
+        CreateSandwich.create_sandwich_order(request, sandwich)
+    return render(request, 'homepage.html')
+
+
+def sandwich_order_delete_view(request):
+    CreateSandwich.delete_all_orders(request)
+    return render(request, 'task_to_complete.html')
+
+
+# Request for create_sandwich_order_view
+def task_to_complete_view(request):
+    orders = CreateSandwich.get_all_orders(request)
+    tasks = CreateSandwich.get_schedule(request, orders)
+    schedule = tasks[0]
+    break_time = tasks[1]
+    return render(request, 'task_to_complete.html', {'orders': orders,
+                                                     'schedule': schedule,
+                                                     'break_time': break_time})
